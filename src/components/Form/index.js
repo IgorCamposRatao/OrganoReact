15e5b697f3e33198734f24f2cccc762041c18e65
IgorCamposRatao <igorcamposratao@gmail.com>
2023-09-14 22:51:09 -0300

    states adicionados ao form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,14 +1,62 @@
+import { useState } from 'react'
+import Button from '../Button'
+import DropdownList from '../DropdownList'
 import TextField from '../TextField'
 import './Form.css'
 
 const Form = () => {
+
+    const roles = [
+        'Tank',
+        'DPS',
+        'Healer',
+        'Controller'
+    ]
+
+    const [name, setName] = useState('')
+    const [clas, setClas] = useState('')
+    const [image, setImage] = useState('')
+    const [role, setRole] = useState('')
+    
+    const onSave = (event) => {
+        event.preventDefault()
+        console.log('Form foi submetido', name, clas, image, role)
+    }
+
     return (
         <section className="form">
-            <form action="">
-                <h2>Preencha os dados para criar o card do personagem</h2>
-                <TextField label="Nome" placeholder="Digite seu nome" />
-                <TextField label="Classe" placeholder="Digite sua classe" />
-                <TextField label="Imagem" placeholder="Digite o endereço da imagem" />
+            <form onSubmit={onSave} >
+                <h2>Fill in the data to create the character card</h2>
+                <TextField 
+                required={true}
+                label="Name" 
+                placeholder="Type your name" 
+                value={name}
+                modify={value => setName(value)}
+                />
+                <TextField 
+                required={true} 
+                label="Class" 
+                placeholder="Type your class" 
+                value={clas}
+                modify={value => setClas(value)}
+                />
+                <TextField 
+                label="Image" 
+                placeholder="Enter the image address" 
+                value={image}
+                modify={value => setImage(value)}
+                />
+                <DropdownList 
+                required={true} 
+                label="Roles" 
+                itens={roles} 
+                value={role}
+                modify={value => setRole(value)}
+                />
+                <Button>
+                    Create Card
+                </Button>
             </form>
         </section>
     )
--- src/components/TextField/index.js
+++ src/components/TextField/index.js
@@ -1,10 +1,18 @@
 
 import './TextField.css'
+
 const TextField = (props) => {
+
+
+
+    const typed = (event) => {
+        props.modify(event.target.value)
+          }
+
     return (
         <div className="field-text">
             <label >{props.label}</label>
-            <input placeholder={props.placeholder} />
+            <input value={props.value} onChange={typed} required={props.required} placeholder={props.placeholder} />
         </div>
     )
 }
